59e16eb9b67bee7bb97a47baa59b36d488d4485f
youssefazzi <tahadart@gmail.com>
2014-09-24 10:03:14 -0400

    Message Bonjour

--- src/githup/GitHup.java
+++ src/githup/GitHup.java
@@ -11,7 +11,7 @@ public class GitHup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("H");
+        System.out.println("Bonjour azzi");
     }
     
 }
